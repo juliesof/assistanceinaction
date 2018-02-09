a5c4e680c84e1dc1b68fce5bf401fe029d6ff7aa
juliesof <juliesof@gmail.com>
2018-02-09 15:28:06 -0500

    small changes to column size in teaser

--- template-parts/content-teaser.php
+++ template-parts/content-teaser.php
@@ -2,15 +2,15 @@
 	<h1 id="teaser-header">Ask Yourself...</h1>
 
 	<div class="row justify-content-md-center teaser-content">
-		<div class="col-xs-12 col-lg-3 first-teaser-question teaser-question">
+		<div class="col-xs-12 col-md-4 first-teaser-question teaser-question">
 			<h3>Where are you?</h3>
 			<div>Where do you want to be in your professional and personal life?</div>
 		</div>
-		<div class="col-xs-12 col-lg-3 teaser-question">
+		<div class="col-xs-12 col-md-4 teaser-question">
 			<h3>Stuck?</h3>
 			<div>Are you aware of the barriers holding you back and how you can eliminate them?</div>
 		</div>
-		<div class="col-xs-12 col-lg-3 teaser-question">
+		<div class="col-xs-12 col-md-4 teaser-question">
 			<h3>Next Steps</h3>
 			<div>Are you ready to set your action plan and next steps for success?</div>
 		</div>
